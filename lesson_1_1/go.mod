81469b564748dd947dbfa6a38755cfc562ba6ee6
Darkmagic666daniil <daniilkitsan2004@gmail.com>
2024-11-13 10:52:36 +0300

    update all

---
+++ lesson_1_1/go.mod
@@ -0,0 +1,3 @@
+module github.com/Darkmagic666daniil/yandex_lms/lesson_1_1
+
+go 1.22.5
---
+++ lesson_1_1/rpn/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/Darkmagic666daniil/yandex_lms/lesson_1_1/rpn/internal/application"
+)
+
+func main() {
+	app := application.New()
+	app.Run()
+}
